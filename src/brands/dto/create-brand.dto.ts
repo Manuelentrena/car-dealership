8f269b6126862b21837199c36f1a76d12d42cdb0
EntrenaDev <manuel.entrena@gmail.com>
2025-02-25 11:51:28 +0100

    feat: :sparkles: Added MinLength validation to CreateBrandDto name property

--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class CreateBrandDto {
   @IsString()
+  @MinLength(1)
   readonly name: string;
 }
