f26602091ca41dcecdcfde54743e1cbc9a1ea167
EntrenaDev <manuel.entrena@gmail.com>
2025-02-25 18:57:48 +0100

    feat: :sparkles: Added ServeStaticModule to serve static files from the public directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
 import { SeedModule } from './seed/seed.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [CarsModule, BrandsModule, SeedModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    CarsModule,
+    BrandsModule,
+    SeedModule,
+  ],
   controllers: [],
   providers: [],
 })
