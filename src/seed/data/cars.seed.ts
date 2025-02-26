2543fa2a55d51882a4716d3243ae52afb257c7c0
EntrenaDev <manuel.entrena@gmail.com>
2025-02-26 12:06:03 +0100

    refactor: :recycle: Updated car seed data to use Car schema and remove UUID generation

--- src/seed/data/cars.seed.ts
+++ src/seed/data/cars.seed.ts
@@ -1,29 +1,23 @@
-import { randomUUID } from 'crypto';
-import { Car } from 'src/cars/types/car.types';
+import { Car } from 'src/cars/schema/car.schema';
 
 export const cars: Car[] = [
   {
-    id: randomUUID(),
     brand: 'Honda',
     model: 'Civic',
   },
   {
-    id: randomUUID(),
     brand: 'Toyota',
     model: 'Corolla',
   },
   {
-    id: randomUUID(),
     brand: 'Ford',
     model: 'Mustang',
   },
   {
-    id: randomUUID(),
     brand: 'Chevrolet',
     model: 'Camaro',
   },
   {
-    id: randomUUID(),
     brand: 'Nissan',
     model: 'GT-R',
   },
