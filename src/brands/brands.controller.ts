636438e61e4b88a511b6fb7ee2b1235eb4b73994
EntrenaDev <manuel.entrena@gmail.com>
2025-02-26 12:28:04 +0100

    refactor: :recycle: Updated BrandsController to use ParseMongoIdPipe for ID parsing and changed delete method to delete

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -6,11 +6,11 @@ import {
   Patch,
   Param,
   Delete,
-  ParseUUIDPipe,
 } from '@nestjs/common';
 import { BrandsService } from './brands.service';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { ParseMongoIdPipe } from 'src/common/pipe/parse-mongo-id.pipe';
 
 @Controller('brands')
 export class BrandsController {
@@ -27,20 +27,20 @@ export class BrandsController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
+  findOne(@Param('id', ParseMongoIdPipe) id: string) {
     return this.brandsService.findOne(id);
   }
 
   @Patch(':id')
   update(
-    @Param('id', ParseUUIDPipe) id: string,
+    @Param('id', ParseMongoIdPipe) id: string,
     @Body() updateBrandDto: UpdateBrandDto,
   ) {
     return this.brandsService.update(id, updateBrandDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.brandsService.remove(id);
+  remove(@Param('id', ParseMongoIdPipe) id: string) {
+    return this.brandsService.delete(id);
   }
 }
