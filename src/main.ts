737b19f56efdf3bf8a260f7af670ae1b8ee4fcc2
EntrenaDev <manuel.entrena@gmail.com>
2025-03-14 13:32:30 +0100

    feat: :heavy_plus_sign: Configure NestJS application with custom logger and global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import logger from './common/config/winston.config';
+import { AllExceptionsFilter } from './common/errors/all-exceptions';
 
 async function main() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger });
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('api.port', 3000);
 
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
