e6bcede572c03f57c2857a35204d0f70d199bc9a
EntrenaDev <manuel.entrena@gmail.com>
2025-03-14 20:30:28 +0100

    feat: :heavy_plus_sign: Enhance Winston logging configuration with colored console output and structured file format

--- src/common/config/winston.config.ts
+++ src/common/config/winston.config.ts
@@ -3,28 +3,35 @@ import * as winston from 'winston';
 
 const logPath =
   process.env.NODE_ENV === 'production'
-    ? '/var/www/app/logs/error.log' // Winston lo interpretará como una ruta relativa dentro de /var/www/app/dist/ (porque ahí se ejecuta el código).
+    ? '/var/www/app/logs/error.log'
     : 'logs/error.log';
 
-const customFormat = winston.format.printf(({ timestamp, level, message }) => {
-  return `[${timestamp}] [${level.toUpperCase()}] ${message}`;
-});
+// 📌 Formato con colores para la consola
+const consoleFormat = winston.format.combine(
+  winston.format.colorize({ all: true }),
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.printf(({ timestamp, level, message }) => {
+    return `[${timestamp}] [${level.toUpperCase()}] ${message}`;
+  }),
+);
+
+// 📌 Formato sin colores para el archivo
+const fileFormat = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.printf(({ timestamp, level, message }) => {
+    return `[${timestamp}] [${level.toUpperCase()}] ${message}`;
+  }),
+);
 
 const logger = WinstonModule.createLogger({
   transports: [
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        customFormat,
-      ),
+      format: consoleFormat, // 🔥 Agregar formato con colores
     }),
     new winston.transports.File({
       filename: logPath,
       level: 'error',
-      format: winston.format.combine(
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        customFormat,
-      ),
+      format: fileFormat, // 🔥 Formato sin colores para el archivo
     }),
   ],
 });
