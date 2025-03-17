9b78d4a79fa61a2723b9e8368dde8a839fcf0d47
EntrenaDev <manuel.entrena@gmail.com>
2025-03-17 12:41:30 +0100

    feat: :heavy_plus_sign: Rename entity to 'auto_image' and add JoinColumn for auto relation

--- database/entities/auto-image.entity.ts
+++ database/entities/auto-image.entity.ts
@@ -1,12 +1,13 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { Auto } from './auto.entity';
 import { BaseEntity } from './base.entity';
 
-@Entity({ name: 'auto' })
+@Entity({ name: 'auto_image' })
 export class AutoImage extends BaseEntity {
   @Column('text', { nullable: false })
   url: string;
 
   @ManyToOne(() => Auto, (auto) => auto.images)
+  @JoinColumn({ name: 'auto_id' })
   auto: Auto;
 }
