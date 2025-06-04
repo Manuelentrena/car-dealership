1fc683f0a39bed906abc2f53299a2ece10ef62a0
entrenadev <manuel.entrena@gmail.com>
2025-06-04 19:16:00 +0200

    feat: :heavy_plus_sign: Add User entity with email, fullName, password, activationToken, isActive, and roles fields

---
+++ database/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from './base.entity';
+
+@Entity({ name: 'user' })
+export class User extends BaseEntity {
+  @Column({
+    type: 'text',
+    unique: true,
+    nullable: false,
+  })
+  email: string;
+
+  @Column({ type: 'text', nullable: false })
+  fullName: string;
+
+  @Column({ type: 'text', nullable: false })
+  password: string;
+
+  @Column({ nullable: true })
+  activationToken?: string;
+
+  @Column({ type: 'boolean', nullable: false, default: true })
+  isActive: boolean;
+
+  @Column({ type: 'text', nullable: false, array: true, default: ['user'] })
+  roles: string[];
+}
