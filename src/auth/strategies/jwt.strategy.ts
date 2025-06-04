ba56f0fb2ca834252a334a18d43ed6addff0b0ae
entrenadev <manuel.entrena@gmail.com>
2025-06-04 19:17:18 +0200

    feat: :heavy_plus_sign: Implement JwtStrategy for JWT authentication handling

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('auth.jwtSecret'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    console.log('Payload recibido en JwtStrategy:', payload);
+    return { userId: payload.sub, email: payload.email, roles: payload.roles };
+  }
+}
