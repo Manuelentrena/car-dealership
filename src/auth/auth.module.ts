9b6b77e3f95492484fb31fad41c9a4f992898a54
entrenadev <manuel.entrena@gmail.com>
2025-06-05 12:24:02 +0200

    feat: :heavy_plus_sign: Export JwtModule and PassportModule from AuthModule for improved accessibility

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -26,5 +26,6 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
