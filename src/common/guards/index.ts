4f6cea3869d58bf83fd8b23f4ca888ca379dcd62
entrenadev <manuel.entrena@gmail.com>
2025-06-05 18:14:27 +0200

    feat: :heavy_plus_sign: Add index exports for auth-user, user-role enums, and user-role guards

---
+++ src/common/guards/index.ts
@@ -0,0 +1,4 @@
+export * from './jwt/jwt-auth.guard';
+export * from './user-role/user-role.guard';
+export * from './user-role/user-role.guard.cache';
+export * from './user-role/user-role.guard.factory';
