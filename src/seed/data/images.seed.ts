8d40520eed7912e2b2f83de45fed89f946d6bcf6
entrenadev <manuel.entrena@gmail.com>
2025-06-07 19:04:36 +0200

    feat: :heavy_plus_sign: Add default public and private image seed data

---
+++ src/seed/data/images.seed.ts
@@ -0,0 +1,8 @@
+export const defaultPublicImage = {
+  url: 'https://imagedelivery.net/vkH-_KAmKeWBBVeCxTyWtQ/5e6f3a8f-57f4-4025-6967-d2a3b3d1aa00/public',
+  isPublic: true,
+};
+export const defaultPrivateImage = {
+  url: 'https://imagedelivery.net/vkH-_KAmKeWBBVeCxTyWtQ/8d4edf49-bbcd-4c7e-bdce-f1887399ce01/public',
+  isPublic: false,
+};
