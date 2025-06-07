7ae540570b469ac7b4f3c1f00e5fe13c35cfaf53
entrenadev <manuel.entrena@gmail.com>
2025-06-07 19:05:28 +0200

    feat: :heavy_plus_sign: Enhance AutoController to include user authentication and pass user data in service calls

--- src/auto/auto.controller.ts
+++ src/auto/auto.controller.ts
@@ -16,7 +16,11 @@ import {
   MAX_IMAGES_BY_AUTO,
   multerImageOptions,
 } from 'src/common/config/files.config';
+import { AuthUser } from 'src/common/decorators';
+import { Auth } from 'src/common/decorators/auth.decorator';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { UserRole } from 'src/common/enums';
+import { AuthUser as AuthUserType } from 'src/common/interface/auth-user.interface';
 import { UUIDOrSlugPipe } from 'src/common/pipe/parse-slug.pipe';
 import { validateTypeImages } from 'src/common/pipe/parse-type-image.pipe';
 import { AutoService } from './auto.service';
@@ -27,17 +31,23 @@ import { UpdateAutoDto } from './dto/update-auto.dto';
 export class AutoController {
   constructor(private readonly autoService: AutoService) {}
 
+  @Auth(UserRole.User)
   @Get()
-  findAll(@Query() paginationDto: PaginationDto) {
-    return this.autoService.findAll(paginationDto);
+  findAll(
+    @AuthUser() user: AuthUserType,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    return this.autoService.findAll(paginationDto, user);
   }
 
+  @Auth(UserRole.User)
   @Get(':term')
   findOne(@Param('term', UUIDOrSlugPipe) term: string) {
     return this.autoService.findOne(term);
   }
 
   @Post()
+  @Auth(UserRole.User)
   @UseInterceptors(
     FileFieldsInterceptor(
       [{ name: 'images', maxCount: MAX_IMAGES_BY_AUTO }],
@@ -45,14 +55,16 @@ export class AutoController {
     ),
   )
   create(
+    @AuthUser() user: AuthUserType,
     @Body() createAutoDto: CreateAutoDto,
     @UploadedFiles() files: { images?: Express.Multer.File[] },
   ) {
     validateTypeImages(files.images);
     createAutoDto.images = files.images;
-    return this.autoService.create(createAutoDto);
+    return this.autoService.create(createAutoDto, user);
   }
 
+  @Auth(UserRole.User)
   @Patch(':id')
   @UseInterceptors(
     FileFieldsInterceptor(
@@ -70,6 +82,7 @@ export class AutoController {
     return this.autoService.update(id, updateAutoDto);
   }
 
+  @Auth(UserRole.User)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.autoService.remove(id);
