5456e54464be8c6a3818629eec98d101182052f2
entrenadev <manuel.entrena@gmail.com>
2025-06-07 19:06:10 +0200

    feat: :heavy_plus_sign: Add authentication to seed execution endpoint

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, ForbiddenException, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Auth } from 'src/common/decorators/auth.decorator';
+import { UserRole } from 'src/common/enums';
 import { SeedService } from './seed.service';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
+  @Auth(UserRole.User)
   @Get()
   execute() {
     // if (process.env.NODE_ENV === 'development') {
