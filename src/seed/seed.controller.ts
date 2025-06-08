40a5a234d204af75a6c6364cb7e49f58d0863bc1
entrenadev <manuel.entrena@gmail.com>
2025-06-08 13:12:12 +0200

    feat: :sparkles: Enhance SeedController with Swagger decorators for improved API documentation

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,23 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Auth } from 'src/common/decorators/auth.decorator';
 import { UserRole } from 'src/common/enums';
 import { SeedService } from './seed.service';
 
+@ApiTags('Seed')
+@ApiBearerAuth()
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Auth(UserRole.User)
   @Get()
+  @ApiOperation({ summary: 'Run database seed and migrations in PostgreSQL' })
+  @ApiResponse({
+    status: 200,
+    description: 'Seed executed successfully.',
+    type: String,
+  })
   execute() {
-    // if (process.env.NODE_ENV === 'development') {
-    // console.log('This endpoint only runs in development');
-    // this.seedService.runSeed();
     this.seedService.runSeedInPostgres();
     return 'Seed executed';
-    // }
-    // throw new ForbiddenException(
-    //   'This endpoint is only available in development mode',
-    // );
   }
 }
